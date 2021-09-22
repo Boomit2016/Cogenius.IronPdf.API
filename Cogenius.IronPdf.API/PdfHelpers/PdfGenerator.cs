055afe84b8e0a77edd07728a6a9e28f60e9a2c6e
Michael Boonaert <michaelboonaert@gmail.com>
2021-09-22 17:24:52 +0200

    Chrome support

--- Cogenius.IronPdf.API/PdfHelpers/PdfGenerator.cs
+++ Cogenius.IronPdf.API/PdfHelpers/PdfGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using IronPdf;
+using IronPdf.Rendering;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -14,6 +15,18 @@ namespace Cogenius.IronPdf.API.PdfHelpers
             var htmlString = await streamReader.ReadToEndAsync();
 
             var renderer = new ChromePdfRenderer();
+            renderer.RenderingOptions.SetCustomPaperSizeInInches(8.5, 11);
+            renderer.RenderingOptions.PaperOrientation = PdfPaperOrientation.Portrait;
+            renderer.RenderingOptions.RenderDelay = 50;
+            renderer.RenderingOptions.CssMediaType = PdfCssMediaType.Screen;
+            renderer.RenderingOptions.Zoom = 100;
+            renderer.RenderingOptions.ViewPortWidth = 1280;
+            renderer.RenderingOptions.CreatePdfFormsFromHtml = true;
+            renderer.RenderingOptions.MarginTop = 10;
+            renderer.RenderingOptions.MarginLeft = 10;
+            renderer.RenderingOptions.MarginRight = 10;
+            renderer.RenderingOptions.MarginBottom = 10;
+
             var pdfDocument = await renderer.RenderHtmlAsPdfAsync(htmlString);
             return pdfDocument.Stream.ToArray();
         }
